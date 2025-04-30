c3ee1d0dd46cb588773401a016d7ac2fbbe283bf
dewantara <dewantara.tirta@gmail.com>
2025-05-01 03:03:50 +0700

    fix: update error variable naming in POST request error handling

--- app/api/campaigns/submission/route.js
+++ app/api/campaigns/submission/route.js
@@ -58,7 +58,7 @@ export async function POST(request) {
 
     // For now, just return the received data
     return NextResponse.json({ submissionId, link, result: {...result?.data?.result} });
-  } catch (error) {
+  } catch (_error) {
     return NextResponse.json({ error: "An error occurred while processing the request." }, { status: 500 });
   }
 }
